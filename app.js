75d8d613c6204f3a0d427cf1e2dc704af86f49e9
setthawut <setthawut.leela@gmail.com>
2019-05-24 18:51:40 +0700

    wowwwww

--- app.js
+++ app.js
@@ -14,6 +14,8 @@ app.use(session({
 }))
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/admin', express.static(path.join(__dirname, 'public')));
+
 
 //Create connection
 const con = mysql.createConnection({
@@ -46,17 +48,21 @@ app.get('/admin', (req, res) => {
     res.sendFile(__dirname+'/adminpage.html')
 });
 
+app.get('/admin/manageaccount', (req, res) => {
+    res.sendFile(__dirname+'/manageaccount.html')
+});
+
 
 
 app.post('/signin',(req, res) => {
-    console.log('Login request;');
+    console.log('Sign in requested...');
     let sql = `SELECT * FROM user_info WHERE email = '${req.body.email}'`;
     let query = con.query(sql, (err, result) => {
-        if(err){ //Query is success
+        if(err){ //Query is not success
             console.log(err);
             res.redirect('/signin');
         }
-        else{ //Query is not success
+        else{ //Query is success
             if(result.length == 0) {//There is no email
                 res.redirect('/signin');
             }
@@ -79,16 +85,36 @@ app.post('/signin',(req, res) => {
             }
         }
     })
-})
+});
+
+app.post('/signup',(req, res) => {
+    console.log('Sign up requested...');
+    var ranking = 'client';
+    let sql = `INSERT INTO user_info(email, fullName, password, rank, userPhone, birthDate)
+                VALUES('${req.body.email}', '${req.body.fullName}', '${req.body.password}',
+                '${ranking}', '${req.body.userPhone}', ${req.body.birthDate})`;
+    let query = con.query(sql, (err, result) => {
+        if(err){ //Query is not success
+            console.log(err);
+            res.redirect('/signup');
+        }
+        else{ //Query is success
+            console.log('sign up successfully...');
+            sess = req.session;
+            sess.email = req.body.email;
+            sess.rank = req.body.rank;
+            sess.fullName = req.body.fullName;
+            res.redirect('/');
+        }
+    });
+});
 
 app.get('/checksession', (req, res) => {
-
     sess = req.session
-
     res.send(JSON.stringify(sess))
 })
 
-app.get('/logout', (req, res) => {
+app.get('/signout', (req, res) => {
     req.session.destroy( (err) => {
         if(err){
             console.log(err)
@@ -99,18 +125,44 @@ app.get('/logout', (req, res) => {
     })
 })
 
-app.get('/getuser', (req, res) => {
-    res.sendFile(__dirname+'/getuser.html')
-})
-
-app.get('/qgetuser', (req, res) => {
-
+app.get('/getaccount', (req, res) => {
     let sql = `SELECT * FROM user_info WHERE 1`;
     let query = con.query(sql, (err, results) => {
         res.send(JSON.stringify(results))
     })
-
 })
+app.get('/signin',(req, res) => {
+    console.log('Sign in requested...');
+    let sql = `SELECT * FROM user_info WHERE email = '${req.body.email}'`;
+    let query = con.query(sql, (err, result) => {
+        if(err){ //Query is not success
+            console.log(err);
+            res.redirect('/signin');
+        }
+        else{ //Query is success
+            if(result.length == 0) {//There is no email
+                res.redirect('/signin');
+            }
+            else {//There is an email
+                if(result[0].password == req.body.password) {//Login is valid
+                    sess = req.session;
+                    sess.email = result[0].email;
+                    sess.rank = result[0].rank;
+                    sess.fullName = result[0].fullName;
+                    if(result[0].rank == 'admin'){
+                        res.redirect('/admin');
+                    }
+                    else if(result[0].rank == 'client'){
+                        res.redirect('/');
+                    }
+                }
+                else { //Log in is invalid
+                    res.redirect('/signin');
+                }
+            }
+        }
+    })
+});
 
 const port = 3000;
 app.listen(port, () => {
